75d522d685d38458d4949e7e0dd936592bd7eae1
Matt Grau <matt.grau@gmail.com>
2020-03-15 04:06:23 +0100

    add alphabetical sort

--- src/regions.tsx
+++ src/regions.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 export default function Regions(props: {
+  alphabetical?: boolean;
   regions: Array<{ region: string; cases: number }>;
   select: (selection: Array<string>) => void;
 }) {
@@ -17,7 +18,19 @@ export default function Regions(props: {
       }
     >
       {props.regions
-        .sort((a, b) => b.cases - a.cases)
+        .sort((a, b) => {
+          if (props.alphabetical) {
+            if (a.region === "All") return -1;
+            if (b.region === "All") return 1;
+            if (a.region === "All except China") return -1;
+            if (b.region === "All except China") return 1;
+            if (a.region < b.region) return -1;
+            if (a.region > b.region) return 1;
+            return 0;
+          } else {
+            return b.cases - a.cases;
+          }
+        })
         .map((value, index) => (
           <option key={index} value={value.region}>
             {value.region}: {value.cases}
